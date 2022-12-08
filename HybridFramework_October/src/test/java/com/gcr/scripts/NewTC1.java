7d773d5a16d6347fa7525bf5a2a709f92c95343d
viritha <viritha@Viritha.hsd1.tn.comcast.net>
2022-12-08 10:09:00 -0500

    Added new test cast - NewTC1

---
+++ HybridFramework_October/src/test/java/com/gcr/scripts/NewTC1.java
@@ -0,0 +1,17 @@
+package com.gcr.scripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.gcr.commands.ActionDriver;
+import com.gcr.config.StartBrowser;
+import com.gcr.reuse.CommonFunctions;
+
+public class NewTC1 extends StartBrowser{
+  @Test
+  public void OnlyLoginLogout() throws Exception {
+	  CommonFunctions cfs = new CommonFunctions();
+	  cfs.login();
+	  cfs.logout();
+  }
+}

7b9319d4ae45808bdf339897757e3e8497c6d99d
viritha <viritha@Viritha.hsd1.tn.comcast.net>
2022-12-09 10:35:13 -0500

    New TC4 Added

---
+++ HybridFramework_October/src/test/java/com/gcr/scripts/TC4.java
@@ -0,0 +1,15 @@
+package com.gcr.scripts;
+
+import org.testng.annotations.Test;
+
+import com.gcr.config.StartBrowser;
+import com.gcr.reuse.BusinessFunction;
+
+public class TC4 extends StartBrowser {
+  @Test
+  public void LoginLogout_WebShop() throws Exception {
+	  BusinessFunction bfs = new BusinessFunction();
+	  bfs.loginWebShop();
+	  Thread.sleep(2000);
+  }
+}
